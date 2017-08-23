9d8baca35340f65941eff2f872aeb4572a570983
Peleg Bar Sapir <tapir@zedat.fu-berlin.de>
2017-08-23 18:33:52 +0200

    added points around atoms for mhp averaging

--- surface.py
+++ surface.py
@@ -5,19 +5,18 @@
 # To do: add ability to read/write frames, neighbor cells
 
 import sys
-from pylab import *
 from prody import *
 import argparse
 import itertools
 import mhplib
+import numpy as np
 
 def point_sphere (c=np.array([.0,.0,.0]), r=1.0, N=5):
     return [np.array([r*np.sin(t)*np.cos(phi),r*np.sin(t)*np.sin(phi),r*np.cos(t)] + c)
             for t in np.arange(.0,2*np.pi,2*np.pi/N)
             for phi in np.arange(.0,np.pi,np.pi/N)]
 
-def mhp (p1=np.array([.0,.0,.0]), p2=np.array([.0,.0,.0]), alpha=.5, f=.0, max_r=5.0):
-    max_r = 10.0 * alpha
+def mhp (p1=np.array([.0,.0,.0]), p2=np.array([.0,.0,.0]), alpha=.5, f=.0, max_r=3.5):
     r = np.linalg.norm(p2-p1)
     if r <= max_r:
         return f * np.exp(-1.0 * alpha * r)
@@ -34,7 +33,7 @@ args = vars (parser.parse_args())
 input_file = args['input']
 subdir = args['subdir']
 select = args['select']
-N_points = args['points']
+N_points = int(args['points'])
 
 pdb = parsePDB(subdir + '/' + input_file + '.pdb')
 psf = parsePSF(subdir + '/' + input_file + '.psf')
@@ -44,8 +43,19 @@ print 'Number of atoms in sub-selection: ', len(pdb_selection)
 
 molecule = [[a.getCoords(), mhplib.vdw_radii[a.getElement()], mhplib.F_val[b.getType()]] for a, b in zip(pdb_selection, psf_selection)]
 A = molecule[0]
-mhp_values = [sum ([mhp(A[0], B[0], .5, B[2]) for B in molecule if B is not A]) for A in molecule]
+mhp_values = []
+k=0
+for atom in molecule:
+    k+=1
+    points = point_sphere(atom[0], atom[1], N_points)
+    mhp_atom = sum([mhp(p, B[0], .5, B[2], atom[1]) for p in points for B in neighbors(atom) if B is not atom])/N_points
+    mhp_values.append(mhp_atom)
+    sys.stderr.write('\rcalculating for atom {} of {} ({} points per atom): {}   '.format(k, len(pdb_selection), N_points, mhp_atom))
+print '\n'
+
 for atom, mhp_val in zip(pdb_selection, mhp_values):
     atom.setBeta (mhp_val)
 
-writePDB (subdir + '/' + input_file + '_mhp.pdb', pdb_selection)
+print 'Writing to ' + input_file + '_mhp_N_' + str(N_points) + '.pdb...'
+writePDB (subdir + '/' + input_file + '_mhp_N_' + str(N_points) + '.pdb', pdb_selection)
+print 'Done.'
